d37a676df68b836ff323d86c9443cabb419e84fc
jenkinsmt <mattjenkins0077@gmail.com>
2024-10-06 00:04:24 -0400

    Final edits to source.py

--- source.py
+++ source.py
@@ -8,20 +8,41 @@
 # ## Topic
 # *What problem are you (or your stakeholder) trying to address?*
 # 📝 <!-- Answer Below -->
+# 
+# The problem I'm trying to address relates to the frequent concussions in professional football, which often times leads to CTE (Chronic Traumatic Encephalopathy) after the playing career. This has a large impact on the brains ability to process and respond to information, as well as many other negatives effects that inhibit ones ability to continue living their life. I'd like to look into current trends in professional football (NCAA/NFL) regarding concussion protocol and see how things have shifted in recent years as technology has advanced. In theory, you'd assume the amount of concussions would be reduced due to the new technology within the helmets to help with traumatic blows to the head area, but it seems as though the number of concussions has stayed relatively the same throughout everything. I'd like to research this more.
 
 # ## Project Question
 # *What specific question are you seeking to answer with this project?*
 # *This is not the same as the questions you ask to limit the scope of the project.*
 # 📝 <!-- Answer Below -->
+# 
+# The questions I'm trying to answer would be: 
+# - Are there a certain amount of career concussions that we can point to as a threshold for CTE? If so, what is that number? 
+# - How much time do players in concussion protocol typically miss? Does this differ by age or position?
 
 # ## What would an answer look like?
 # *What is your hypothesized answer to your question?*
 # 📝 <!-- Answer Below -->
+# 
+# An answer may look like: 
+# - At 6 concussions and beyond in an athletes playing career, they are statistically likely to be diagnosed with CTE.
+# - At 4 career concussions, the likelihood of being diagnosed with CTE jumps significantly.
+# - In the NFL, players at skill positions (QB, WR, RB, DB) typically miss more time while being in concussion protocol.
+# - Players between ages 21-25 spend significantly less time in concussion protocol in comparison to players aged 26-30.
 
 # ## Data Sources
 # *What 3 data sources have you identified for this project?*
 # *How are you going to relate these datasets?*
 # 📝 <!-- Answer Below -->
+# 
+# Data Source 1: https://www.prosportstransactions.com/football/Search/Search.php
+# - NFL injury database, allowing people to view all injury designations throughout past NFL seasons.
+# 
+# Data Source 2: https://www.nfl.com/injuries/
+# - Similar to the first data source but is updated with the current season, which could be beneficial to see if any trends have shifted or started this year that differ from previous years.
+# 
+# Data Source 3: https://www.kaggle.com/code/mpwolke/nfl-concussions/notebook
+# - NFL concussion data beyond simply the injury designation. Showcases # of weeks in concussion list among many other things.
 
 # ## Approach and Analysis
 # *What is your approach to answering your project question?*
@@ -38,9 +59,9 @@
 # *What resources and references have you used for this project?*
 # 📝 <!-- Answer Below -->
 
-# In[ ]:
+# In[2]:
 
 
 # ⚠️ Make sure you run this cell at the end of your notebook before every submission!
-get_ipython().system('jupyter nbconvert --to python python-exercises.ipynb')
+get_ipython().system('jupyter nbconvert --to python source.ipynb')
 
